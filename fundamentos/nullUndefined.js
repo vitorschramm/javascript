121ef80b174424cabe814f84cd705a188774a10d
Vitor Hugo <vitorhugojoinville@gmail.com>
2020-01-27 16:02:55 -0300

    conceitos null e undefined

---
+++ fundamentos/nullUndefined.js
@@ -0,0 +1,15 @@
+let valor //não inicializada
+console.log(valor)
+
+
+valor = null
+console.log(valor)
+
+const produto = {}
+console.log(produto.preco)
+console.log(produto)
+
+produto.preco = 3.5
+
+console.log(produto.preco)
+console.log(produto)
