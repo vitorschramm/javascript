3b2ccbfaee9118332725f0ff0331940576e031a7
Vitor Hugo <vitorhugojoinville@gmail.com>
2020-01-27 14:08:42 -0300

    template String

---
+++ fundamentos/templateString.js
@@ -0,0 +1,17 @@
+const nome = "Rebeca"
+const concatenacao = 'Ola' + nome + "!"
+
+console.log ("Olá " + nome + "!")
+
+const template = `
+
+    Olá
+    ${nome}!`
+
+console.log(concatenacao, template)
+
+//expressoes...
+console.log(`1 + 1 = ${1 + 1}`)
+
+const up = texto => texto.toUpperCase()
+console.log (`Ei...${up('cuidado')}!`)
