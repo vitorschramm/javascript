6642748ce03f26541cacd97369b74a7f1337e86d
Vitor Hugo <vitorhugojoinville@gmail.com>
2020-01-27 15:41:03 -0300

    objetos

---
+++ fundamentos/objetos.js
@@ -0,0 +1,15 @@
+//objeto é uma coleção de chave e valor
+const prod1 = {}
+prod1.nome = "celular ultra mega"
+prod1.preco = 4999.99
+prod1['Desconto Legal'] = 0.40 //evitar atributos com espaço
+console.log(prod1)
+
+const prod2 = {
+nome: "Camisa polo",
+preco: 79.98,
+tamanho: "G",
+}
+console.log(prod2)
+
+
